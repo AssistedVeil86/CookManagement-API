a5c9885acfa75651110e5410783e4b3d4acfb650
AssistedVeil <wilmerfernando2006@gmail.com>
2026-01-11 16:38:26 -0600

    ChangeUserRecordsDateFilter

--- CookManagement.VSA/Features/Users/GetUserRecords/GetUserRecordsHandler.cs
+++ CookManagement.VSA/Features/Users/GetUserRecords/GetUserRecordsHandler.cs
@@ -31,12 +31,19 @@ namespace CookManagement.VSA.Features.Users.GetUserRecords
                 ? InventoryType.Cocina
                 : InventoryType.Bar;
 
-            var (startOfDayUtc, endOfDayUtc) = _timeZoneService.GetTodayBoundariesInUtc(timeZoneId);
+            //var (startOfDayUtc, endOfDayUtc) = _timeZoneService.GetTodayBoundariesInUtc(timeZoneId);
+
+            //var baseQuery = _context.UserRecords.AsNoTracking()
+            //    .Where(r => r.UserId == user.Id
+            //            && r.CreatedAt >= startOfDayUtc
+            //            && r.CreatedAt < endOfDayUtc
+            //            && r.InventoryType == inventoryType);
+
+            var utcRequestedDate = requestedDate.ToUniversalTime();
 
             var baseQuery = _context.UserRecords.AsNoTracking()
                 .Where(r => r.UserId == user.Id
-                        && r.CreatedAt >= startOfDayUtc
-                        && r.CreatedAt < endOfDayUtc
+                        && r.CreatedAt == utcRequestedDate
                         && r.InventoryType == inventoryType);
 
             var userRecords = await baseQuery
