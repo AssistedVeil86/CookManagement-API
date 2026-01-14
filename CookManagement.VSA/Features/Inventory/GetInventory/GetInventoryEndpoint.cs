fcfb9b10eed006e0275d7554e4951f8733622437
AssistedVeil <wilmerfernando2006@gmail.com>
2026-01-13 23:17:43 -0600

    AddInventoryFilterByCategory

--- CookManagement.VSA/Features/Inventory/GetInventory/GetInventoryEndpoint.cs
+++ CookManagement.VSA/Features/Inventory/GetInventory/GetInventoryEndpoint.cs
@@ -9,14 +9,14 @@ namespace CookManagement.VSA.Features.Inventory.GetInventory
         public static RouteGroupBuilder MapGetInventoryEndpoint(this RouteGroupBuilder groupBuilder)
         {
             groupBuilder.MapGet("",
-                    async (GetInventoryHandler handler, ClaimsPrincipal user, InventoryType? inventoryType,
+                    async (GetInventoryHandler handler, ClaimsPrincipal user, InventoryType? inventoryType, string? category,
                         int page = 1, int pageSize = 12) =>
                     {
                         var userId = user.GetUserId();
                         var userRole = user.GetUserRole();
 
                         var result =
-                        await handler.HandleAsync(userId, userRole, page, pageSize, inventoryType);
+                        await handler.HandleAsync(userId, userRole, page, pageSize, inventoryType, category);
 
                         return Results.Ok(result);
                     })
