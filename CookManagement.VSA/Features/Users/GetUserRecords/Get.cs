ab96f2fada62b336137426fa8d5e0fafa709ad38
AssistedVeil86 <wilmerfernando2006@gmail.com>
2026-07-06 11:24:32 -0600

    Add Optional Query Param Inventory Type to GetUserRecords Feature

--- CookManagement.VSA/Features/Users/GetUserRecords/Get.cs
+++ CookManagement.VSA/Features/Users/GetUserRecords/Get.cs
@@ -1,7 +1,7 @@
-using CookManagement.VSA.Infrastructure.Data;
-using CookManagement.VSA.Infrastructure.TimeZones;
 using CookManagement.VSA.Domain.Enums;
 using CookManagement.VSA.Domain.Exceptions;
+using CookManagement.VSA.Infrastructure.Data;
+using CookManagement.VSA.Infrastructure.TimeZones;
 using Microsoft.EntityFrameworkCore;
 
 namespace CookManagement.VSA.Features.Users.GetUserRecords;
@@ -19,9 +19,10 @@ public static class GetUserRecordsEndpoint
 
     private static async Task<IResult> Handler(
         GetUserRecordsHandler handler, string userName,
-        DateTime requestedDate, string timeZoneId, int page = 1, int pageSize = 12)
+        DateTime requestedDate, string timeZoneId, InventoryType? inventoryType, int page = 1, int pageSize = 12)
     {
-        var results = await handler.HandleAsync(userName, requestedDate, timeZoneId, page, pageSize);
+        var results = await handler
+            .HandleAsync(userName, requestedDate, timeZoneId, inventoryType, page, pageSize);
         return Results.Ok(results);
     }
 }
@@ -31,16 +32,16 @@ internal sealed class GetUserRecordsHandler(
     TimeZoneService timeZoneService)
 {
     public async Task<RecordsPaginatedResponse> HandleAsync(
-        string userName, DateTime requestedDate, string timeZoneId, int page, int pageSize)
+        string userName, DateTime requestedDate, string timeZoneId,
+        InventoryType? inventoryType, int page, int pageSize)
     {
         var user = await context.Users.FirstOrDefaultAsync(u => u.Name == userName);
 
         if (user is null)
             throw new CustomNotFoundException("Ese usuario no existe");
 
-        var inventoryType = user.Role == UserRole.Cocina
-            ? InventoryType.Cocina
-            : InventoryType.Bar;
+        var resolvedInventoryType = inventoryType
+            ?? (user.Role == UserRole.Cocina ? InventoryType.Cocina : InventoryType.Bar);
 
         var (startOfDayUtc, endOfDayUtc) = timeZoneService.GetCurrentDateBoundariesInUtc(timeZoneId, requestedDate);
 
@@ -48,7 +49,7 @@ internal sealed class GetUserRecordsHandler(
             .Where(r => r.UserId == user.Id
                     && r.CreatedAt >= startOfDayUtc
                     && r.CreatedAt < endOfDayUtc
-                    && r.InventoryType == inventoryType);
+                    && r.InventoryType == resolvedInventoryType);
 
         var userRecords = await baseQuery
             .OrderBy(r => r.ProductCode)
